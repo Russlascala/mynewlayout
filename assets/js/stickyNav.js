2d853222cca7f840378ac139380ec716ea83e721
Russ Lascala <russlascala@gmail.com>
2018-05-30 16:59:01 -0400

    fixed the nav so that the double nav is no longer an issue. also styled it with less

--- assets/js/stickyNav.js
+++ assets/js/stickyNav.js
@@ -1,16 +1,13 @@
 window.onscroll = function() {stickyNav()};
 
 var navbar = document.getElementsByClassName("navbar");
-var navDropdown = document.getElementById("navDropdown");
 var initiate = document.getElementById("slideshow");
 var sticky = initiate.offsetTop;
 
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
     navbar[0].classList.add("sticky");
-    navDropdown.classList.add("stickyDrop");
   } else {
     navbar[0].classList.remove("sticky");
-    navDropdown.classList.remove("stickyDrop");
   }
 }
